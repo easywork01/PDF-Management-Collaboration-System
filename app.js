9f7749d26e7116c350cd515c711744555579947e
easywork01 <pranjal4real@gmail.com>
2023-07-22 20:28:04 +0530

    hashing and salting using bcrypt

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;  
+
 
 const app = express();
 
@@ -8,8 +13,16 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
 
+const userScema = {
+    email: String,
+    password: String
+}//userSchema is based on simple JsObject which has two fields
 
+const User = new mongoose.model("User", userScema)
+//making a new model using userSchema name of model is "User"(singluar form)
+//next we make a new collection called "Users"(plural) to store each entry of our model
 
 app.get("/",function(req,res){
     res.render("home");
@@ -24,21 +37,37 @@ app.get("/login",function(req,res){
 
 
 
-app.post("/register", funtion(req,res){
-    const newUser = new User({
-        email = req.body.username,
-        password = req.body.password
-    });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("dash");
-        }
-    });
-});
 
-app.post("login", function(req, res){
+
+app.post("/register", function(req, res) {
+  
+    bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+      try {
+        if (err) {
+          console.log(err);
+          return res.status(500).send("Error occurred while hashing the password.");
+        }
+  
+        // Store hash in your password DB.
+        const newUser = new User({
+          email: req.body.username,
+          password: hash
+        });
+  
+        await newUser.save();
+        res.render("dash");
+      } catch (err) {
+        console.log(err);
+        res.status(500).send("Error occurred while saving the user.");
+      }
+    });
+  });
+  
+
+
+
+
+app.post("/login", function(req, res){
     const username = req.body.username;
     const password = req.body.password;
 
@@ -46,11 +75,17 @@ app.post("login", function(req, res){
         if(err){
             console.log(err);
         }else{
-            if(foundUser{
-                if(foundUser.password === password){
-                    res.render("dash");
-                }
-            });
+            if(foundUser){
+                // Load hash from your password DB.
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    // result == true
+                    if(result === true){
+                        res.render("dash");
+                    }
+                });
+                    
+                
+            };
         }
     });
 });
